b679ec746a23546ec28bfcc9f1aa7a55bc73004b
HAA02 <88357250+HAA02@users.noreply.github.com>
2022-01-18 18:05:09 +0900

    Update RouletteController.cs

--- Assets/RouletteController.cs
+++ Assets/RouletteController.cs
@@ -15,7 +15,7 @@ public class RouletteController : MonoBehaviour
     void Update()
     {
         // 클릭하면 회전 속도를 설정한다.
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             this.rotSpeed = 10;
         }
@@ -23,7 +23,9 @@ public class RouletteController : MonoBehaviour
         // 회전 속도만큼 룰렛을 회전시킨다.
         transform.Rotate(0, 0, this.rotSpeed);
 
-        // 룰렛을 감속시킨다.
-        this.rotSpeed *= 0.96f;
+      
+            this.rotSpeed *= 0.96f;
+        
+        
     }
 }
